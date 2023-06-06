7f429d2f65ce9986a860a67aa29e0bf68f247c6e
Acer <sqratty@mail.ru>
2023-06-06 23:25:11 +0300

    Prac 1

--- ProjectWeb/store/urls.py
+++ ProjectWeb/store/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 
 from .views import CartView, ShopView, ProductSingleView
 
+app_name = 'store'
 
 urlpatterns = [
-    path('', CartView.as_view()),
-    path('', ShopView.as_view()),
-    path('', ProductSingleView.as_view())
+    path('cart/', CartView.as_view(), name='cart'),
+    path('', ShopView.as_view(), name='shop'),
+    path('product/<int:id>/', ProductSingleView.as_view(), name='product')
 ]
