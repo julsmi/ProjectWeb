efe775e6ce7f8f6cdc07eb5e260fffe72ca7944c
Acer <sqratty@mail.ru>
2023-06-17 12:18:16 +0300

    Prac 3_1

--- ProjectWeb/store/admin.py
+++ ProjectWeb/store/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Category, Discount, Cart
+from .models import Product, Category, Discount, Cart, Profile
 
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Discount)
 admin.site.register(Cart)
+admin.site.register(Profile)
